e80527736513996a318bff5cf4c1ec1131b190d1
skapur524-a11y <skapur524@gmail.com>
2026-09-14 17:18:08 +0530

    Add files via upload

---
+++ 01_problem1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+   // int length = 3;
+   // int breadth = 6;
+   int length, breadth;
+   printf("Enter length\n");
+   scanf("%d", &length);
+
+    printf("Enter breagth\n");
+    scanf("%d", &breadth);
+
+    printf("The area of this rectangle is %d", length*breadth);
+    return 0;
+}
+
+
