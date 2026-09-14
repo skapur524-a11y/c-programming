9745bf4a7020a93e4a35cb39280cd7805b19b179
skapur524-a11y <skapur524@gmail.com>
2026-09-14 17:27:52 +0530

    Fix typo in prompt for rectangle length input

--- 01_problem1.c
+++ 01_problem1.c
@@ -4,10 +4,10 @@ int main(){
    // int length = 3;
    // int breadth = 6;
    int length, breadth;
-   printf("Enter length\n");
+   printf("Enter the length\n");
    scanf("%d", &length);
 
-    printf("Enter breagth\n");
+    printf("Enter the breagth\n");
     scanf("%d", &breadth);
 
     printf("The area of this rectangle is %d", length*breadth);
