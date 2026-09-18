f3847eff56cd0e418e2084b403b202e1bc4c8e02
Shashi <skapur524@gmail.com>
2026-09-18 09:54:00 +0530

    Updated length message

--- 01_problem1.c
+++ 01_problem1.c
@@ -4,7 +4,7 @@ int main(){
    // int length = 3;
    // int breadth = 6;
    int length, breadth;
-   printf("Enter the length\n");
+   printf("Enter the length of rectangle:\n");
    scanf("%d", &length);
 
     printf("Enter the breagth\n");
