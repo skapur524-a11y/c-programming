0851ed9b94302fa2d4058e238e6da1ade6ced1f7
Shashi <skapur524@gmail.com>
2026-09-20 17:19:21 +0530

    Change rectangle length prompt

--- 01_problem1.c
+++ 01_problem1.c
@@ -4,7 +4,7 @@ int main(){
    // int length = 3;
    // int breadth = 6;
    int length, breadth;
-   printf("Enter the length of the rectangle:");
+   printf("Enter rectangle length:");
    scanf("%d", &length);
 
     printf("Enter the breadth of rectangle:");
