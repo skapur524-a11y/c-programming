ebb284808f0247c4b474c5a2458f71d9c832e293
Shashi <skapur524@gmail.com>
2026-09-21 16:22:48 +0530

    Improve rectangle length prompt

--- 01_problem1.c
+++ 01_problem1.c
@@ -4,7 +4,7 @@ int main(){
    // int length = 3;
    // int breadth = 6;
    int length, breadth;
-   printf("Enter rectangle length:");
+   printf("Enter the rectangle length:");
    scanf("%d", &length);
 
     printf("Enter the breadth of rectangle:");
